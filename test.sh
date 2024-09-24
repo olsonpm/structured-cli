5ae019044280383393e2da35635f58685b37bbd4
Phil Olson <olsonpm@users.noreply.github.com>
2024-09-24 17:40:24 -0500

    misc updates
    - remove webpack and separate entrypoints es5/es6
    - add license
    - add package-lock.json
    - update package.json info
    - remove unused test.sh
    - bump version

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-. "$(locate nvm.sh)"
-
-./build.sh \
-  && nvm run 6.0.0 ./node_modules/mocha/bin/mocha --color=always tests/es6.js \
-  && nvm run 0.10.0 ./node_modules/mocha/bin/mocha --color=always tests/es5.js
-
-nvm use node
